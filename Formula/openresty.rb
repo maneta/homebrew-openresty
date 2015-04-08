c11df0c7c091d6fb85ce1c91e0ed37393a491124
kikito <kikito@gmail.com>
2015-04-08 11:40:30 +0200

    Remove all the options which we don't use in APItools

--- Formula/openresty.rb
+++ Formula/openresty.rb
@@ -9,21 +9,9 @@ class Openresty < Formula
   end
 
   depends_on 'pcre'
-  depends_on 'postgresql' => :optional
-  depends_on 'geoip' => :optional
 
   # openresty options
-  option 'without-luajit', "Compile *without* support for the Lua Just-In-Time Compiler"
-  option 'with-postgresql', "Compile with support for direct communication with PostgreSQL database servers"
-  option 'with-iconv', "Compile with support for converting character encodings"
-
   option 'with-debug', "Compile with support for debug logging but without proper gdb debugging symbols"
-
-  # nginx options
-  option 'with-webdav', "Compile with ngx_http_dav_module"
-  option 'without-gunzip', "Compile without ngx_http_gunzip_module"
-  option 'with-geoip', "Compile with ngx_http_geoip_module"
-  option 'with-stub_status', "Compile with ngx_http_stub_status_module"
 
   skip_clean 'logs'
 
@@ -35,33 +23,19 @@ class Openresty < Formula
       "--sbin-path=#{bin}/nginx",
       "--conf-path=#{etc}/openresty/nginx.conf",
       "--pid-path=#{var}/run/openresty.pid",
-      "--lock-path=#{var}/openresty/nginx.lock"
+      "--lock-path=#{var}/openresty/nginx.lock",
+      "--with-http_gunzip_module"
     ]
-
-    args << "--with-http_dav_module" if build.with? 'webdav'
-    args << "--with-http_gunzip_module" unless build.without? 'gunzip'
-    args << "--with-http_geoip_module" if build.with? 'geoip'
-    args << "--with-http_stub_status_module" if build.with? 'stub_status'
 
     # Debugging mode, unfortunately without debugging symbols
     if build.with? 'debug'
       args << '--with-debug'
       args << '--with-dtrace-probes'
       args << '--with-no-pool-patch'
-
-      # this allows setting of `debug.sethook` in luajit
-      unless build.without? 'luajit'
-        args << '--with-luajit-xcflags=-DLUAJIT_ENABLE_CHECKHOOK'
-      end
+      args << '--with-luajit-xcflags=-DLUAJIT_ENABLE_CHECKHOOK'
 
       opoo "Openresty will be built --with-debug option, but without debugging symbols. For debugging symbols you have to compile it by hand."
     end
-
-    # OpenResty options
-    args << "--with-lua51" if build.without? 'luajit'
-
-    args << "--with-http_postgres_module" if build.with? 'postgresql'
-    args << "--with-http_iconv_module" if build.with? 'iconv'
 
     system "./configure", *args
 
