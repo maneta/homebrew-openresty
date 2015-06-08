4d4f81137e2abd887e3f5422019a154bfee183b3
kikito <kikito@gmail.com>
2015-06-08 15:39:58 +0200

    update luarocks version. fix conf issues

--- Formula/luarocks.rb
+++ Formula/luarocks.rb
@@ -5,8 +5,8 @@ class Luarocks < Formula
   homepage 'https://rocks.moonscript.org/'
 
   stable do
-    url 'http://keplerproject.github.io/luarocks/releases/luarocks-2.2.1.tar.gz'
-    sha1 '82b858889e31ec0eb4d05ce7ea3a72fdf5403aad'
+    url 'http://keplerproject.github.io/luarocks/releases/luarocks-2.2.2.tar.gz'
+    sha1 'eb867374e5a11edb705fd9fab3b47b083a9e55a6'
   end
 
   depends_on "apitools/openresty/openresty"
@@ -16,13 +16,13 @@ class Luarocks < Formula
     openresty_luajit_folder          = File.join(openresty_folder, 'luajit')
     openresty_luajit_include_folder  = File.join(openresty_luajit_folder, 'include', 'luajit-2.1')
 
+    system "./configure",
+           "--prefix=#{prefix}",
+           "--rocks-tree=#{HOMEBREW_PREFIX}",
+           "--with-lua=#{openresty_luajit_folder}",
+           "--with-lua-include=#{openresty_luajit_include_folder}",
+           "--lua-suffix=jit-2.1.0-alpha"
 
-    args = ["--prefix=#{prefix}",
-            "--with-lua=#{openresty_luajit_folder}",
-            "--lua-suffix=jit-2.1.0-alpha",
-            "--with-lua-include=#{openresty_luajit_include_folder}"]
-
-    system "./configure", *args
     system "make", "build"
     system "make", "install"
   end
