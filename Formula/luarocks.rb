55d323da0a6bd456e4e134913a2da2b1591d5ad2
Michal Cichra <michal@cichra.cz>
2016-09-07 14:50:59 +0200

    [luarocks] bump luarocks to 2.3.0

--- Formula/luarocks.rb
+++ Formula/luarocks.rb
@@ -5,14 +5,14 @@ class Luarocks < Formula
   homepage 'https://rocks.moonscript.org/'
 
   stable do
-    url 'http://keplerproject.github.io/luarocks/releases/luarocks-2.2.2.tar.gz'
-    sha1 'eb867374e5a11edb705fd9fab3b47b083a9e55a6'
+    url 'https://github.com/keplerproject/luarocks/archive/v2.3.0.tar.gz'
+    sha256 '92c014889ec6a09c4bb492df6b7f7be784110d6abe031e16418342781ca5c5ce'
   end
 
-  depends_on "apitools/openresty/openresty"
+  depends_on "homebrew/nginx/openresty"
 
   def install
-    openresty_folder                 = Formula["openresty"].opt_prefix
+    openresty_folder                 = Formula["homebrew/nginx/openresty"].opt_prefix
     openresty_luajit_folder          = File.join(openresty_folder, 'luajit')
     openresty_luajit_include_folder  = File.join(openresty_luajit_folder, 'include', 'luajit-2.1')
 
@@ -21,7 +21,8 @@ class Luarocks < Formula
            "--rocks-tree=#{HOMEBREW_PREFIX}",
            "--with-lua=#{openresty_luajit_folder}",
            "--with-lua-include=#{openresty_luajit_include_folder}",
-           "--lua-suffix=jit-2.1.0-alpha"
+           "--lua-version=5.1",
+           "--lua-suffix=jit"
 
     system "make", "build"
     system "make", "install"
